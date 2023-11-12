626b02b2d45e2f6ae59eacc877ad42a513638105
l-yohai <yohan9612@naver.com>
2023-11-12 22:06:51 +0900

    style: apply pre-commit

--- data/archive_lotto_numbers.py
+++ data/archive_lotto_numbers.py
@@ -1,20 +1,22 @@
-import requests
 import json
 
+import requests
 from tqdm import tqdm
+
 
 def fetch_lotto_numbers(draw_no):
     # 대한민국 로또 API URL
-    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={draw_no}'
-    
+    url = f"https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={draw_no}"
+
     # API 요청
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         # JSON 데이터 파싱
         return response.json()
     else:
         return None
+
 
 def save_lotto_numbers_to_jsonl(start_draw, current_draw):
     # 로또 번호들을 저장할 리스트
@@ -25,14 +27,15 @@ def save_lotto_numbers_to_jsonl(start_draw, current_draw):
         lotto_data = fetch_lotto_numbers(draw_no)
         if lotto_data:
             lotto_numbers_list.append(lotto_data)
-    
+
     # JSONL 파일로 저장
-    with open('data/lotto_numbers.jsonl', 'w') as file:
+    with open("data/lotto_numbers.jsonl", "w") as file:
         for lotto_data in lotto_numbers_list:
             json_record = json.dumps(lotto_data)
-            file.write(json_record + '\n')
+            file.write(json_record + "\n")
 
     return "Lotto numbers saved to lotto_numbers.jsonl"
+
 
 # 시작 회차와 현재 회차 설정 (예: 1부터 1092회차까지)
 start_draw = 1
@@ -41,4 +44,3 @@ current_draw = 1092
 # JSONL 파일로 저장
 save_result = save_lotto_numbers_to_jsonl(start_draw, current_draw)
 save_result
-
--- data/get_latest_lotto_number.py
+++ data/get_latest_lotto_number.py
@@ -1,37 +1,43 @@
-import requests
 import json
 import os
+
+import requests
+
 
 def fetch_latest_draw_no(jsonl_file):
     last_draw_no = 0
     if os.path.exists(jsonl_file):
-        with open(jsonl_file, 'r') as file:
+        with open(jsonl_file, "r") as file:
             for line in file:
                 lotto_data = json.loads(line.strip())
                 last_draw_no = max(last_draw_no, lotto_data.get("drwNo", 0))
     return last_draw_no
 
+
 def fetch_lotto_numbers(draw_no):
-    url = f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={draw_no}'
+    url = f"https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={draw_no}"
     response = requests.get(url)
     return response.json() if response.status_code == 200 else None
 
+
 def append_to_jsonl(jsonl_file, data):
-    with open(jsonl_file, 'a') as file:
+    with open(jsonl_file, "a") as file:
         json_record = json.dumps(data)
-        file.write(json_record + '\n')
+        file.write(json_record + "\n")
+
 
 def main():
-    jsonl_file = 'data/lotto_numbers.jsonl'
+    jsonl_file = "data/lotto_numbers.jsonl"
     last_draw_no = fetch_latest_draw_no(jsonl_file)
     new_draw_no = last_draw_no + 1
     new_lotto_data = fetch_lotto_numbers(new_draw_no)
-    
+
     if new_lotto_data:
         append_to_jsonl(jsonl_file, new_lotto_data)
         print(f"Added draw number {new_draw_no} to {jsonl_file}")
     else:
         print(f"Failed to fetch data for draw number {new_draw_no}")
+
 
 if __name__ == "__main__":
     main()
